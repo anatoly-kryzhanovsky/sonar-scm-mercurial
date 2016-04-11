b8f7028485a403503cec920c2f3ac1fdaaebff2f
Tom <thomas.verin@sonarsource.com>
2016-04-11 11:39:10 +0200

    added missing files and rewrote travis.sh

--- travis.sh
+++ travis.sh
@@ -2,26 +2,23 @@
 
 set -euo pipefail
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local                                                    
-  source ~/.local/bin/install                                                                                                                                    
-}                                                                                                                                                                
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
 
-installTravisTools
+case "$TEST" in
 
-#regular_mvn_build_deploy_analyze
+CI)
+  regular_mvn_build_deploy_analyze
+  ;;
 
-SONAR_PROJECT_VERSION=`maven_expression "project.version"`
- 
-  # Do not deploy a SNAPSHOT version but the release version related to this build
-  set_maven_build_version $TRAVIS_BUILD_NUMBER
- 
-  # the profile "deploy-sonarsource" is defined in parent pom v28+
-  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
-    -Pcoverage-per-test,deploy-sonarsource \
-    -Dmaven.test.redirectTestOutputToFile=false \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.login=$SONAR_TOKEN \
-    -Dsonar.projectVersion=$SONAR_PROJECT_VERSION \
-    -B -e -V
+
+*)
+  echo "Unexpected TEST mode: $TEST"
+  exit 1
+  ;;
+
+esac
