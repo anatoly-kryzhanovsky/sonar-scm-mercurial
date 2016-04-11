c3de4879e1bd58b974547ce408826369aa634b85
Tom <thomas.verin@sonarsource.com>
2016-04-11 11:07:37 +0200

    Setup QA

--- travis.sh
+++ travis.sh
@@ -4,21 +4,24 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v25 | tar zx --strip-components 1 -C ~/.local                                                    
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local                                                    
   source ~/.local/bin/install                                                                                                                                    
 }                                                                                                                                                                
 
-mvn verify -B -e -V                                                                                                                                              
+installTravisTools
 
-if [ "${RUN_ITS}" == "true" ]
-then
-  installTravisTools
+#regular_mvn_build_deploy_analyze
 
-  if [ "${SQ_VERSION}" == "DEV" ]
-  then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its  
-  mvn -Dsonar.runtimeVersion=$SQ_VERSION -Dmaven.test.redirectTestOutputToFile=false verify
-fi
+SONAR_PROJECT_VERSION=`maven_expression "project.version"`
+ 
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+ 
+  # the profile "deploy-sonarsource" is defined in parent pom v28+
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+    -Pcoverage-per-test,deploy-sonarsource \
+    -Dmaven.test.redirectTestOutputToFile=false \
+    -Dsonar.host.url=$SONAR_HOST_URL \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.projectVersion=$SONAR_PROJECT_VERSION \
+    -B -e -V
